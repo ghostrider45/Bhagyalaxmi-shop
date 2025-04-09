971aa82bcc667452133590315199626c92383264
Ganesh Choudhary <98375431+ghostrider45@users.noreply.github.com>
2025-04-09 18:08:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 // Initialize Firebase Admin SDK with service account file
 try {
-    const serviceAccount = require('./service_account_key.json');
+    const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
 
     // Initialize the app with admin credentials if not already initialized
     if (admin.apps.length === 0) {
